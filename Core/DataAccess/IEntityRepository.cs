ed14caba8578da272d04484217486c5ae1aa28b3
eterzioglu <eterzioglu8@gmail.com>
2021-02-22 00:03:50 +0300

    9. Gun

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,16 +1,16 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //Generic repository oluşturduk..
     //Generic constraint - kısıt vericez T tipine.
     //class olabilir demek değil bu referans tip olabilir demek ve T sadece IEntitye ait olabilir..
     //new() : newlenebilir olmalı.
-    public interface IEntityRepository<T> where T:class,IEntity ,new()
+    public interface IEntityRepository<T> where T:class,IEntity ,new() 
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
