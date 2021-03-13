9212f3d2428874da577221a089d8a05186a1f493
eterzioglu <eterzioglu8@gmail.com>
2021-03-13 14:42:42 +0300

    11. Ders

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -32,8 +32,7 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
             //iş kodları..
-
-            if(DateTime.Now.Hour == 17)
+            if(DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
